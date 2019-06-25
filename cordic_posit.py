c64341dc4949783cbd0c124985afd852b396a88f
mshachnai <m.shachnai@rutgers.edu>
2019-06-25 01:57:03 -0400

    updating

--- cordic_posit.py
+++ cordic_posit.py
@@ -71,15 +71,21 @@ def cordic_itr(ang, n):
     print("sin of %d (in degrees) = " %ang, bf.sin(deg_to_rad(ang), bf.precision(PREC)))
     print("tan of %d (in degrees) = " %ang, bf.tan(deg_to_rad(ang), bf.precision(PREC)))
     
-    swap = float(x) 
-    swap = float(x) 
-    swap = float(x) 
-    x_pp = bf.BigFloat.exact(swap)
+    swapx = float(x) 
+    swapy = float(y) 
+    swaptan = float(y/x) 
+    cos_p = bf.BigFloat.exact(swapx)
+    sin_p = bf.BigFloat.exact(swapy)
+    tan_p = bf.BigFloat.exact(swaptan)
     #print("bigfloat exact: ", swap, x_pp)
-    x_mpfr = bf.cos((math.pi/9), bf.precision(PREC))
-    y_mpfr = bf.sin((math.pi/9), bf.precision(PREC))
-    z_mpfr = bf.tan((math.pi/9), bf.precision(PREC))
 
+    cos_mpfr = bf.cos(deg_to_rad(ang), bf.precision(PREC))
+    sin_mpfr = bf.sin(deg_to_rad(ang), bf.precision(PREC))
+    tan_mpfr = bf.tan(deg_to_rad(ang), bf.precision(PREC))
+    
+    cos_diff = (cos_mpfr-cos_p) / cos_mpfr
+    sin_diff = (sin_mpfr-sin_p) / sin_mpfr
+    tan_diff = (tan_mpfr-tan_p) / tan_mpfr
     #print("error of cos of %d (in degrees) = %.200f" %(ang, x_mpfr-x_pp))
     #print("error of sin of %d (in degrees) = %.200f" %(ang, y_mpfr-y))
     #print("error of tan of %d (in degrees) = %.200f" %(ang, z_mpfr-z))
@@ -88,12 +94,13 @@ def cordic_itr(ang, n):
     #verify angle is correct
     #print("angle = ", math.degrees(math.atan(y/x)))
     
-    arr1 = [ang, "%.20E" %x, "%.200f" %y_mpfr, "%.200f" %x, "%.200f" %x, "%.200f" %x, "%.200f"
-            %x,"%.200f" %x,"%.200f" %x,"%.200f" %x,"%.200f" %x,"%.200f" %x,"%.200f"
-            %x, "%.200f" %x, "%.200f" %x,]
+    arr1 = [ang, "%.100f" %y, "%.200f" %sin_diff, "%.100f" %x, "%.100f" %cos_diff,
+            "%.100f" %(y/x), "%.100f" %tan_diff,
+            "%.100f" %x,"%.100f" %x,"%.100f" %x,"%.100f" %x,"%.100f" %x,"%.100f"
+            %x, "%.100f" %x, "%.100f" %x,]
 
     with open('posit.csv', 'a') as f:
-        #f.write("%.200f\n" % x) 
+        #        f.write("%.200f\n" % x_mpfr) 
         csvfile = csv.writer(f)
         csvfile.writerow(arr1)
 
@@ -101,14 +108,13 @@ def cordic_itr(ang, n):
 
 if __name__ == '__main__':
     fields = ['Angle', 'Posit-sin', 'Error', 'Posit-cos', 'Error', 'Posit-tan', 'Error', 'Float-sin',
-            'Error', 'Float-cos', 'Error', 'Float-tan', 'Error', 'Mathlib', 'MPFR']
+            'Error', 'Float-cos', 'Error', 'Float-tan', 'Error', 'Mathlib-sin',
+            'MPFR-sin']
 
     with open('posit.csv', 'w') as f:
             csvfile = csv.writer(f)
             csvfile.writerow(fields)
 
-    cordic_itr(20, 40)
-    cordic_itr(20, 40)
     cordic_itr(20, 40)
 
     f.close()
