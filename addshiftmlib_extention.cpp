f1ae5890db8321b5f395a23247397602e8562370
mshachnai <m.shachnai@rutgers.edu>
2019-08-30 14:30:17 -0400

    updated functions to work for posits - need to change precomuted tables of hyperbolic/exp/ln

--- addshiftmlib_extention.cpp
+++ addshiftmlib_extention.cpp
@@ -131,11 +131,9 @@ void sqrt_cordic ( posit32 x, int n, posit32& sqrtx){
 	sqrtx = y;
 
 
-
+//evaluates the exponential function using the CORDIC method.
 posit32 exp_cordic ( posit32 x, int n ){
 # define A_LENGTH 25
-
-
 //change precomputed table to posits
   double a[A_LENGTH] = { 
     1.648721270700128, 
@@ -237,6 +235,118 @@ posit32 exp_cordic ( posit32 x, int n ){
       fx = fx * e;
     }
   }
+
+  delete [] w;
+
+  return fx;
+# undef A_LENGTH
+}
+
+
+
+//evaluates the natural logarithm using the CORDIC method.
+posit32 ln_cordic ( posit32 x, int n )
+# define A_LENGTH 25
+
+//change precomputed table to posits
+  double a[A_LENGTH] = {
+    1.648721270700128, 
+    1.284025416687742, 
+    1.133148453066826, 
+    1.064494458917859, 
+    1.031743407499103, 
+    1.015747708586686, 
+    1.007843097206488, 
+    1.003913889338348, 
+    1.001955033591003, 
+    1.000977039492417, 
+    1.000488400478694, 
+    1.000244170429748, 
+    1.000122077763384, 
+    1.000061037018933, 
+    1.000030518043791, 
+    1.0000152589054785, 
+    1.0000076294236351, 
+    1.0000038147045416, 
+    1.0000019073504518, 
+    1.0000009536747712, 
+    1.0000004768372719, 
+    1.0000002384186075, 
+    1.0000001192092967, 
+    1.0000000596046466, 
+    1.0000000298023228 };
+  posit32 ai;
+  posit32 e = 2.718281828459045;
+  posit32 fx;
+  int i;
+  int k;
+  posit32 poweroftwo;
+  posit32 *w;
+
+  if ( x <= 0.0 )
+  {
+    cerr << "\n";
+    cerr << "LN_CORDIC - Fatal error!\n";
+    cerr << "  Input argument X <= 0.0\n";
+    exit ( 1 );
+  }
+
+  k = 0;
+
+  while ( e <= x )
+  {
+    k = k + 1;
+    x = x / e;
+  }
+
+  while ( x < 1.0 )
+  {
+    k = k - 1;
+    x = x * e;
+  }
+//
+//  Determine the weights.
+//
+  w = new posit32[n];
+
+  for ( i = 0; i < n; i++ )
+  {
+    w[i] = 0.0;
+
+    if ( i < A_LENGTH )
+    {
+      ai = a[i];
+    }
+    else
+    {
+      ai = 1.0 + ( ai - 1.0 ) / 2.0;
+    }
+
+    if ( ai < x )
+    {
+      w[i] = 1.0;
+      x = x / ai;
+    }
+  }
+
+  x = x - 1.0;
+
+  x = x
+    * ( 1.0 - ( x / 2.0 ) 
+    * ( 1.0 + ( x / 3.0 ) 
+    * ( 1.0 -   x / 4.0 )));
+//
+//  Assemble
+//
+  poweroftwo = 0.5;
+
+  for ( i = 0; i < n; i++ )
+  {
+    x = x + w[i] * poweroftwo;
+    poweroftwo = poweroftwo / 2.0;
+  }
+
+  fx = x + ( posit32 ) ( k );
 
   delete [] w;
 
