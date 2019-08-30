728cb3d65f3c0b15c82011b88e21a0961a84de24
mshachnai <m.shachnai@rutgers.edu>
2019-08-30 14:22:39 -0400

    added exp() and ln() functions - still need to add precomuted tables and change to posit32

--- addshiftmlib_extention.cpp
+++ addshiftmlib_extention.cpp
@@ -86,11 +86,6 @@ void posit_cosh(posit32 z, int n, posit32& cosh){
 
 
 //function to compute sqrt() 
-//  Parameters:
-//
-//    Input, posit32 X, the number whose square root is desired.
-//    Input, int N, the number of iterations to take.
-//    Output, double SQRT_CORDIC, the approximate square root of X.
 void sqrt_cordic ( posit32 x, int n, posit32& sqrtx){
 	posit32 poweroftwo;
 	posit32 y;
@@ -134,4 +129,117 @@ void sqrt_cordic ( posit32 x, int n, posit32& sqrtx){
 	}
 	
 	sqrtx = y;
+
+
+
+posit32 exp_cordic ( posit32 x, int n ){
+# define A_LENGTH 25
+
+
+//change precomputed table to posits
+  double a[A_LENGTH] = { 
+    1.648721270700128, 
+    1.284025416687742, 
+    1.133148453066826, 
+    1.064494458917859, 
+    1.031743407499103, 
+    1.015747708586686, 
+    1.007843097206488, 
+    1.003913889338348, 
+    1.001955033591003, 
+    1.000977039492417, 
+    1.000488400478694, 
+    1.000244170429748, 
+    1.000122077763384, 
+    1.000061037018933, 
+    1.000030518043791, 
+    1.0000152589054785, 
+    1.0000076294236351, 
+    1.0000038147045416, 
+    1.0000019073504518, 
+    1.0000009536747712, 
+    1.0000004768372719, 
+    1.0000002384186075, 
+    1.0000001192092967, 
+    1.0000000596046466, 
+    1.0000000298023228 };
+  posit32 ai;
+  posit32 e = 2.718281828459045;
+  posit32 fx;
+  int i;
+  posit32 poweroftwo;
+  posit32 *w;
+  int x_int;
+  posit32 z;
+
+  x_int = ( int ) ( floor ( x ) );
+//
+//  Determine the weights.
+//
+  poweroftwo = 0.5;
+  z = x - ( posit32 ) ( x_int );
+
+  w = new posit32[n];
+
+  for ( i = 0; i < n; i++ )
+  {
+    w[i] = 0.0;
+    if ( poweroftwo < z )
+    {
+      w[i] = 1.0;;
+      z = z - poweroftwo;
+    }
+    poweroftwo = poweroftwo / 2.0;
+  }
+//
+//  Calculate products.
+//
+  fx = 1.0;
+
+  for ( i = 0; i < n; i++ )
+  {
+    if ( i < A_LENGTH )
+    {
+      ai = a[i];
+    }
+    else
+    {
+      ai = 1.0 + ( ai - 1.0 ) / 2.0;
+    }
+
+    if ( 0.0 < w[i] )
+    {
+      fx = fx * ai;
+    }
+  }
+//
+//  Perform residual multiplication.
+//
+  fx = fx                     
+    * ( 1.0 + z           
+    * ( 1.0 + z / 2.0 
+    * ( 1.0 + z / 3.0 
+    * ( 1.0 + z / 4.0 ))));
+//
+//  Account for factor EXP(X_INT).
+//
+  if ( x_int < 0 )
+  {
+    for ( i = 1; i <= -x_int; i++ )
+    {
+      fx = fx / e;
+    }
+  }
+  else
+  {
+    for ( i = 1; i <= x_int; i++ )
+    {
+      fx = fx * e;
+    }
+  }
+
+  delete [] w;
+
+  return fx;
+# undef A_LENGTH
 }
